0e707967fd17c8b957bc346a9354db8db7fb9e11
jgwehr <dm.nota@gmail.com>
2018-05-09 22:25:07 -0500

    Implementation of web.search

--- tutorial.js
+++ tutorial.js
@@ -1,15 +1,36 @@
 const { IncomingWebhook, WebClient } = require('@slack/client');
+
 const SLACK_WEBHOOK_TUTORIAL = process.env.SLACK_WEBHOOK_TUTORIAL;
+const SLACK_OAUTH = new WebClient(process.env.SLACK_OAUTH);
 console.log(`SLACK_WEBHOOK_TUTORIAL=${SLACK_WEBHOOK_TUTORIAL}`);
 
 
+
+const web = new WebClient(process.env.SLACK_OAUTH);
 const timeNotification = new IncomingWebhook(SLACK_WEBHOOK_TUTORIAL);
 const currentTime = new Date().toTimeString();
+
 timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
   if (error) {
     return console.error(error);
   }
   console.log('Notification sent');
+  console.log('Waiting a few seconds for search indexes to update...');
+  setTimeout(() => {
+    console.log('Calling search.messages');
+    web.search.messages({ query: currentTime })
+      .then(resp => {
+        if (resp.messages.total > 0) {
+          console.log('First match:', resp.messages.matches[0]);
+        } else {
+          console.log('No matches found');
+        }
+      })
+      .catch(console.error)
+  }, 12000);
 });
+
+
+
 
 console.log('now I end');
