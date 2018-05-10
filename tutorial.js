7cd8e3293ef217be25063664c848d13c17bac827
jgwehr <dm.nota@gmail.com>
2018-05-09 23:19:08 -0500

    Send message as a bot

--- tutorial.js
+++ tutorial.js
@@ -9,27 +9,18 @@ console.log(`SLACK_TOKEN=${SLACK_TOKEN}`);
 //Utility initilization
 const web = new WebClient(SLACK_TOKEN);
 const timeNotification = new IncomingWebhook(SLACK_WEBHOOK_TUTORIAL);
-const currentTime = new Date().toTimeString();
 
-timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
-  if (error) {
-    return console.error(error);
-  }
-  console.log('Notification sent');
-  console.log('Waiting a few seconds for search indexes to update...');
+//Channel. Should come from incoming requests
+const conversationId = 'CAHDCR2LD';
 
 
-  setTimeout(() => {
-    console.log('Calling search.messages');
-    web.search.messages({ query: 'Central Daylight Time' })
-      .then(resp => {
-        if (resp.messages.total > 0) {
-          console.log('First match:', resp.messages.matches[0]);
-        } else {
-          console.log('No matches found');
-        }
-      })
-      .catch(console.error)
-  }, 12000);
-
-});
+//Send a message
+web.chat.postMessage({
+  channel: conversationId,
+  text: 'I AM GROOT'
+})
+  .then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);
