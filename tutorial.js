bf75b6f23856545b96c353229f64ca9ffa3e9a17
jgwehr <dm.nota@gmail.com>
2018-05-09 23:06:16 -0500

    Updated OAUTH Token env

--- tutorial.js
+++ tutorial.js
@@ -1,12 +1,13 @@
 const { IncomingWebhook, WebClient } = require('@slack/client');
 
 const SLACK_WEBHOOK_TUTORIAL = process.env.SLACK_WEBHOOK_TUTORIAL;
-const SLACK_OAUTH = new WebClient(process.env.SLACK_OAUTH);
+const SLACK_TOKEN_OAUTH = new WebClient(process.env.SLACK_TOKEN_OAUTH);
 console.log(`SLACK_WEBHOOK_TUTORIAL=${SLACK_WEBHOOK_TUTORIAL}`);
+console.log(`SLACK_TOKEN_OAUTH=${SLACK_TOKEN_OAUTH}`);
 
 
 
-const web = new WebClient(process.env.SLACK_OAUTH);
+const web = new WebClient(process.env.SLACK_TOKEN_OAUTH);
 const timeNotification = new IncomingWebhook(SLACK_WEBHOOK_TUTORIAL);
 const currentTime = new Date().toTimeString();
 
@@ -16,9 +17,11 @@ timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
   }
   console.log('Notification sent');
   console.log('Waiting a few seconds for search indexes to update...');
+
+
   setTimeout(() => {
     console.log('Calling search.messages');
-    web.search.messages({ query: currentTime })
+    web.search.messages({ query: 'Central Daylight Time' })
       .then(resp => {
         if (resp.messages.total > 0) {
           console.log('First match:', resp.messages.matches[0]);
@@ -28,9 +31,5 @@ timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
       })
       .catch(console.error)
   }, 12000);
+
 });
-
-
-
-
-console.log('now I end');
