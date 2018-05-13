4cfffd561f70daf6ec2fd08f0eb307d1aaa4957c
jgwehr <dm.nota@gmail.com>
2018-05-12 19:53:12 -0500

    Migrated from tutorial to server

--- server.js
+++ server.js
@@ -10,20 +10,29 @@ const SLACK_CLIENT_SECRET       = process.env.SLACK_CLIENT_SECRET;
 const SLACK_TOKEN_OAUTH         = process.env.SLACK_TOKEN_OAUTH;
 const SLACK_TOKEN_BOT           = process.env.SLACK_TOKEN_BOT;
 const SLACK_VERIFICATION_TOKEN  = process.env.SLACK_VERIFICATION_TOKEN;
-const SLACK_WEBHOOK_TUTORIAL    = process.env.SLACK_WEBHOOK_TUTORIAL;
-// const SLACK_TOKEN               = process.env.SLACK_TOKEN_BOT || SLACK_TOKEN_OAUTH;
+const SLACK_TOKEN               = process.env.SLACK_TOKEN_BOT || SLACK_TOKEN_OAUTH;
 
 console.log('Environment Variables loaded');
 
-//Express init
+//init
 var app = express();
+const slackWeb = new WebClient(SLACK_TOKEN);
 
+
+//SlackChannel. Should come from incoming requests
+const conversationId = 'CAHDCR2LD';
+
+
+
+
+//Point to the public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 //Route setup
 app.route('/')
   .get(function (req, res) {
-    res.send('Hello World')
+    res.status(200).send('Hello World')
   })
   .post(function (req, res) {
     res.send('Got a POST request')
@@ -36,18 +45,32 @@ app.route('/')
   });
 
 app.route('/rally/')
-   .post(function(err,req, res){
-      if(err){
-         res.send('Look now I handle errors');
-      }
-      res.send('Received POST request to /rally/')
-   });
+  .get(function (req, res) {
 
+    slackWeb.chat.postMessage({
+      channel: conversationId,
+      text: 'I AM GROOT'
+    }).then((res) => {
+        console.log('Message sent: ', res.ts);
+    }).catch(console.error);
+
+
+    res.send('Found Rally');
+  })
+  .post(function(req, res){
+    if(err){
+       res.send('Look now I handle errors');
+    }
+    res.send('Received POST request to /rally/')
+  });
+
+
+
+//404
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+});
 
 //Finish up and debug
-app.listen(PORT, function(){
-   var host = app.address().address
-   var port = app.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-});
+app.listen(PORT, () => console.log(`PORT=${PORT}`));
 console.log(`NODE_ENV=${process.env.NODE_ENV}`);
