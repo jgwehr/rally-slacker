675a0d3ebc8f83122cedbf2b389a13b80965b9ea
jgwehr <dm.nota@gmail.com>
2018-05-12 20:23:22 -0500

    Successful botkit!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+![node](https://img.shields.io/node/v/passport.svg)
+
 # rally-slacker
 A bot to expose Rally (CA Agile Central) projects to Slack
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const { IncomingWebhook, WebClient } = require('@slack/client');
 const PORT = process.env.SERVER_PORT || 5000;
 
+//Custom Requirements
+var customIntegration = require('./lib/slack_custom_integration');
+
+
 //Slack setup and config
 const SLACK_CLIENT_ID           = process.env.SLACK_CLIENT_ID;
 const SLACK_CLIENT_SECRET       = process.env.SLACK_CLIENT_SECRET;
@@ -19,10 +23,18 @@ var app = express();
 const slackWeb = new WebClient(SLACK_TOKEN);
 
 
-//SlackChannel. Should come from incoming requests
+//Slack config. Should come from incoming requests
 const conversationId = 'CAHDCR2LD';
 
+//Botkit config
+var config = {
+    json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
+};
+var controller = customIntegration.configure(SLACK_TOKEN, config, onInstallation);
 
+controller.hears('hello', 'direct_message', function (bot, message) {
+    bot.reply(message, 'Hello!');
+});
 
 
 //Point to the public folder
@@ -32,36 +44,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Route setup
 app.route('/')
   .get(function (req, res) {
-    res.status(200).send('Hello World')
-  })
-  .post(function (req, res) {
-    res.send('Got a POST request')
-  })
-  .put(function (req, res) {
-    res.send('Got a PUT request at /user')
-  })
-  .delete(function (req, res) {
-    res.send('Got a DELETE request at /user')
-  });
-
-app.route('/rally/')
-  .get(function (req, res) {
-
     slackWeb.chat.postMessage({
       channel: conversationId,
       text: 'I AM GROOT'
     }).then((res) => {
         console.log('Message sent: ', res.ts);
     }).catch(console.error);
-
-
     res.send('Found Rally');
   })
   .post(function(req, res){
-    if(err){
-       res.send('Look now I handle errors');
-    }
-    res.send('Received POST request to /rally/')
+    res.send('Received POST request to /rally')
+  })
+  .put(function (req, res) {
+    res.send('Got a PUT request at /rally')
+  })
+  .delete(function (req, res) {
+    res.send('Got a DELETE request at /rally')
   });
 
 
@@ -74,3 +72,23 @@ app.use(function (req, res, next) {
 //Finish up and debug
 app.listen(PORT, () => console.log(`PORT=${PORT}`));
 console.log(`NODE_ENV=${process.env.NODE_ENV}`);
+
+
+
+
+
+
+
+
+function onInstallation(bot, installer) {
+    if (installer) {
+        bot.startPrivateConversation({user: installer}, function (err, convo) {
+            if (err) {
+                console.log(err);
+            } else {
+                convo.say('I am a bot that has just joined your team');
+                convo.say('You must now /invite me to a channel so that I can be of use!');
+            }
+        });
+    }
+}
