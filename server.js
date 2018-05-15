d7b187ac3eb65e9b19a32d7023f2aa5ef8c23ece
jgwehr <dm.nota@gmail.com>
2018-05-14 20:43:17 -0500

    Factored Botkit init out
    Separated the instantiation of botkit out to allow for independent bot creation

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.SERVER_PORT || 5000;
 const DEBUG_TOGGLE = process.env.DEBUG_TOGGLE;
 
 //Custom Requirements
-var slack_custom_integration  = require('./lib/slack_custom_integration');
+var botkit  = require('./lib/botkit');
 
 //Slack setup and config
 const SLACK_CLIENT_ID           = process.env.SLACK_CLIENT_ID;
@@ -42,7 +42,10 @@ var bot_config = {
     debug: DEBUG_TOGGLE,
     disable_startup_messages: false
 };
-var slackRealtimeController = slack_custom_integration.configure(SLACK_TOKEN, bot_config, onInstallation);
+var botkitController = botkit.configure(SLACK_TOKEN, bot_config);
+
+var botRealTime = require('./lib/slack_custom_integration').configure(botkitController,SLACK_TOKEN);
+
 
 
 /*
@@ -53,21 +56,5 @@ console.log("Loading bot's skills...");
 //This loads all skill modules in the /skills/ directory
 var skillsPath = require("path").join(__dirname, "skills");
 require("fs").readdirSync(skillsPath).forEach(function(file) {
-  require(skillsPath + '\\' + file)(slackRealtimeController);
+  require(skillsPath + '\\' + file)(botkitController);
 });
-
-
-
-
-function onInstallation(bot, installer) {
-    if (installer) {
-        bot.startPrivateConversation({user: installer}, function (err, convo) {
-            if (err) {
-                console.log(err);
-            } else {
-                convo.say('I am a bot that has just joined your team');
-                convo.say('You must now /invite me to a channel so that I can be of use!');
-            }
-        });
-    }
-}
