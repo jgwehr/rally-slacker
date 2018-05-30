816d04ea131134d55e33b229d959b4dadab776dd
jgwehr <dm.nota@gmail.com>
2018-05-30 18:43:23 -0500

    Announce the team on RTM user story

--- skills/hear_user_story.js
+++ skills/hear_user_story.js
@@ -1,7 +1,7 @@
 module.exports = function(controller) {
   var regex_userstory = /(US\s?[\d]{4})/gi;
   controller.hears(regex_userstory, ['ambient'], function (bot, message) {
-    console.log("I think I heard a user story!");
+    console.log("A User Story came from team: "+bot.identifyTeam());
     bot.replyInThread(message, 'I think I heard a user story!');
   });
 };
